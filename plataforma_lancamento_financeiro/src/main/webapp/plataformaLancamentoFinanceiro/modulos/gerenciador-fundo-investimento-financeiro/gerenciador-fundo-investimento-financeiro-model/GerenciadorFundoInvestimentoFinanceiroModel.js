5c29501295d86943bd1344dfacd69a88257d41ed
Repository Desenvolvimento <repository.desenvolvimento@gmail.com>
2019-01-28 08:29:49 -0200

    [Plataforma de Lançamento Financeiro] Implementação da Prótotipação do Módulo de Gestão de Fundos de Investimentos

---
+++ plataforma_lancamento_financeiro/src/main/webapp/plataformaLancamentoFinanceiro/modulos/gerenciador-fundo-investimento-financeiro/gerenciador-fundo-investimento-financeiro-model/GerenciadorFundoInvestimentoFinanceiroModel.js
@@ -0,0 +1,15 @@
+class GerenciadorFundoInvestimentoFinanceiroModel {
+
+    constructor() {
+        this.favorecidoFundoInvestimento = "";
+        this.nomeFundoInvestimento = "";
+        this.classificacaoFundoInvestimento = "";
+        this.categoriaFundoInvestimento = "";
+        this.valorAplicacaoMinimaFundoInvestimento = "";
+        this.mesAplicacaoFundoInvestimento = "";
+        this.anoAplicacaoFundoInvestimento = "";
+        this.rendimentoPrevistoFundoInvestimento = "";
+        this.inAplicacaoExteriorFundoInvestimento = "";
+    }
+
+}
