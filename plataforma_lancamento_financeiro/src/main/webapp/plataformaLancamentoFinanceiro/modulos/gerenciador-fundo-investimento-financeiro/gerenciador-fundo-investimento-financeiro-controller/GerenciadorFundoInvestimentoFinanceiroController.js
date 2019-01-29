79952dc83383e3d1a1d7814d6ae60ff884a42525
Repository Desenvolvimento <repository.desenvolvimento@gmail.com>
2019-01-29 20:02:38 -0200

    [Plataforma de Lançamento Financeiro] Implementação da Prótotipação do Módulo de Gestão de Fundos de Investimentos

--- plataforma_lancamento_financeiro/src/main/webapp/plataformaLancamentoFinanceiro/modulos/gerenciador-fundo-investimento-financeiro/gerenciador-fundo-investimento-financeiro-controller/GerenciadorFundoInvestimentoFinanceiroController.js
+++ plataforma_lancamento_financeiro/src/main/webapp/plataformaLancamentoFinanceiro/modulos/gerenciador-fundo-investimento-financeiro/gerenciador-fundo-investimento-financeiro-controller/GerenciadorFundoInvestimentoFinanceiroController.js
@@ -1,15 +1,25 @@
-class GerenciadorFundoInvestimentoFinanceiroModel {
+class GerenciadorFundoInvestimentoFinanceiroController {
 
-    constructor() {
-        this.favorecidoFundoInvestimento = "";
-        this.nomeFundoInvestimento = "";
-        this.classificacaoFundoInvestimento = "";
-        this.categoriaFundoInvestimento = "";
-        this.valorAplicacaoMinimaFundoInvestimento = "";
-        this.mesAplicacaoFundoInvestimento = "";
-        this.anoAplicacaoFundoInvestimento = "";
-        this.rendimentoPrevistoFundoInvestimento = "";
-        this.inAplicacaoExteriorFundoInvestimento = "";
+    // Responsavel por adicionar um modelo
+    salvarInvestimentoFinanceiro(event) {
+
+        event.preventDefault(); // responsavel por cancelar o comportamento padrao do formulario
+
+        let instituicaoFinanceiraInput = document.querySelector('#instituicaoFinanceira');
+        let nomeInvestimentoInput = document.querySelector('#nomeInvestimento');
+        let tipoMercadoFinanceiroInput = document.querySelector('#tipoMercadoFinanceiro');
+        let riscoInvestimentoInput = document.querySelector('#riscoInvestimento');
+        let dataAquisicaoFundoInput = document.querySelector('#dataAquisicaoFundo');
+        let validadeInvestimentoInput = document.querySelector('#validadeInvestimento');
+        let valorInvestimentoInput = document.querySelector('#valorInvestimento');
+        let percentualRendimentoPrevistoInput = document.querySelector('#percentualRendimentoPrevisto');
+        
+        console.log('Invocando controller...');
+
+        console.log(instituicaoFinanceiraInput);
+        console.log(nomeInvestimentoInput);
+        console.log(tipoMercadoFinanceiroInput);
+
     }
 
 }
